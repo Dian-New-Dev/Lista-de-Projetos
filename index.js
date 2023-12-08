f6584bf4a2dab29d189f2bcf0188ead910614e63
Diego <diannewdev@gmail.com>
2023-12-08 14:15:09 -0400

    better cleaner design

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const FemButton = document.getElementById('FEM-title');
 const FemContent = document.getElementById('FEM-Projects');
+
+FemButton.addEventListener('mouseover', function () {
+    FemButton.style.cursor = 'pointer';
+})
+
 FemButton.addEventListener('click', function () {
     showcontent(FemContent);
 })
 
 const JS30Button = document.getElementById('JS30-title');
 const JS30content = document.getElementById('JS30-Projects')
+
+JS30Button.addEventListener('mouseover', function () {
+    JS30Button.style.cursor = 'pointer';
+})
+
 JS30Button.addEventListener('click', function () {
     showcontent(JS30content);
 })
@@ -20,6 +30,20 @@ function showcontent(content) {
     
 
 }
+
+let i = 1;
+const panelToMove = document.getElementById('sideButtons');
+panelToMove.addEventListener('click', function() {
+    i++;
+    if (i % 2 === 0) {
+        panelToMove.style.transform = 'translateX(75px)';
+    } else {
+        panelToMove.style.transform = 'translateX(-1px)';
+
+    }
+    
+  });
+  
 
 
 
